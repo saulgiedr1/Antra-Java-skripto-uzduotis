42eec1e41cd11c6637a1143365cc0385bfca7622
saulgiedr1 <saulgiedr@gmail.com>
2022-06-08 19:01:53 +0300

    pirma užduotis taisyta1

--- script.js
+++ script.js
@@ -1,32 +1,31 @@
 window.onload =function() {
 
-
-    let zodis;
-    let zodis1;
-    let zodis2;
-    let raide;
-    let raide1;
-    let raide2;
-
-    zodis="automobilis";
-    zodis1="laivas";
-    zodis2="namas";
     
+   
+    let zodis="bananas";
+    let zodis1="plaukai";
+    let zodis2="namas";
+    let raide=zodis[0];
+    let raide1=zodis1[0];
+    let raide2=zodis2[0];
+    let pirmoji;
+    let pirmoji1;
+    let pirmoji2;     
+        
+    pirmoji=pirmaRaide(raide);
+    pirmoji1=pirmaRaide(raide1);
+    pirmoji2=pirmaRaide(raide2)
 
-    raide=pirmaRaide(zodis[0]);
-    raide1=pirmaRaide(zodis1[0]);
-    raide2=pirmaRaide(zodis2[0]);
 
-
-    console.log(raide);
-    console.log(raide1);
-    console.log(raide2);
+    console.log(pirmoji);
+    console.log(pirmoji1);
+    console.log(pirmoji2);       
 
 }
-
-    function pirmaRaide(a) {
-
-    return   a ;
+function pirmaRaide(a) {
+    return a;
     
 }
+
+
 
