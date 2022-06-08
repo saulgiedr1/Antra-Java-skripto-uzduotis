25e393a938d0e7e5ef1624e4686fa44b8c4f6ea1
saulgiedr1 <saulgiedr@gmail.com>
2022-06-08 17:14:55 +0300

    pirma u=duotis taisyta

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
 window.onload =function() {
 
-    let zodis="automobilis";
-    let raide=zodis[0];
+
+    let zodis;
+    let zodis1;
+    let zodis2;
+    let raide;
+    let raide1;
+    let raide2;
+
+    zodis="automobilis";
+    zodis1="laivas";
+    zodis2="namas";
+    
+
+    raide=pirmaRaide(zodis[0]);
+    raide1=pirmaRaide(zodis1[0]);
+    raide2=pirmaRaide(zodis2[0]);
 
 
     console.log(raide);
+    console.log(raide1);
+    console.log(raide2);
+
 }
-function parodytiPirmaRaide(zodis)
-{
-    return(raide);
+
+    function pirmaRaide(a) {
+
+    return   a ;
+    
 }
 
