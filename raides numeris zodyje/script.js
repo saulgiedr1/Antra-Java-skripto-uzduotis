f04bb52e10b850d9446b6c8f42836b3486e77274
saulgiedr1 <saulgiedr@gmail.com>
2022-06-09 10:34:08 +0300

    raides skaicius zodyje

---
+++ raides numeris zodyje/script.js
@@ -0,0 +1,17 @@
+window.onload=function() {
+
+    let zodis= "lokys";    
+    
+    let raide= zodis.indexOf("s");
+   
+    let paskutine;   
+
+    paskutine=paskutineRaide(raide);   
+
+  console.log(paskutine); 
+
+
+}
+function  paskutineRaide(a){
+    return a;
+}
