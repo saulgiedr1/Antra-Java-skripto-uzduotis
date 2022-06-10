047046677688350c2e689b60ccf77b01f9dd0b18
saulgiedr1 <saulgiedr@gmail.com>
2022-06-10 09:03:57 +0300

    skaiciaus modulis

--- SLU-2-skaiciaus-modulis/script.js
+++ SLU-2-skaiciaus-modulis/script.js
@@ -1,2 +1,22 @@
-window.onload=function() {
+window.onload=function() { 
+    let skaicius;
+    let skaicius1;
+    skaicius=-200;
+    skaicius1=-1.347;
+
+    modulis=skaiciausModulis(skaicius);
+    console.log(modulis);
+
+    modulisKubu=teigiamasKubas(skaicius1);     
+    console.log(modulisKubu);
+
 }
+
+function skaiciausModulis(a){
+    return Math.abs(a);
+}
+
+function teigiamasKubas(a){
+    return Math.pow(Math.abs(a), 3);
+}
+
