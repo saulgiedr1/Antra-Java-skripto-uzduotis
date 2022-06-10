71995860296c22ac0f57df7cfb149380e9fd90e1
saulgiedr1 <saulgiedr@gmail.com>
2022-06-10 14:40:57 +0300

    kainos didinimas procentais

---
+++ Penktadienio uzduotys/U3-zodis-atvirksciai/U5-kainos didinimas procentais/script.js
@@ -0,0 +1,26 @@
+window.onload=function(){
+    let kaina;
+    let kaina1;
+    let procentai;
+    let procentai1;
+    let galutine;
+    let galutine1;
+
+    kaina=120;
+    kaina1=2000;
+    procentai=13;
+    procentai1=1.258;
+
+    galutine=kainosDidinimas(kaina, procentai);
+    galutine1=kainosDidinimas(kaina1, procentai1);
+
+
+    console.log(galutine);
+    console.log(galutine1);
+
+
+
+}
+function kainosDidinimas(a, b){
+    return a+(a*b)/100; 
+}
