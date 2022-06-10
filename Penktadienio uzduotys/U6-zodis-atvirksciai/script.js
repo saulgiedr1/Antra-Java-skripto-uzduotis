259c87a2737f2c46c9b439fa00967f78aebeb00d
saulgiedr1 <saulgiedr@gmail.com>
2022-06-10 12:41:50 +0300

    apsuktas zodis

---
+++ Penktadienio uzduotys/U6-zodis-atvirksciai/script.js
@@ -0,0 +1,20 @@
+window.onload=function() {
+    let zodis;
+    let zodis1;
+    
+    let apsuktas;
+    let apsuktas1
+
+    zodis="kava"
+    zodis1="java"
+
+
+    apsuktas=apsuktasZodis(zodis);
+    apsuktas1=apsuktasZodis(zodis1);
+    console.log(apsuktas+" "+apsuktas1);
+    
+}
+
+function apsuktasZodis(a){
+    return a[3]+a[2]+a[1]+a[0];
+}
