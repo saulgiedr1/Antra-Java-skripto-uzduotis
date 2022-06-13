b65cfb51db889e2913b172f405f504ed20d3fc75
saulgiedr1 <saulgiedr@gmail.com>
2022-06-13 11:57:22 +0300

    fibonacio skaicius

--- Penktadienio uzduotys/U8 -fibonnacio skaicius/script.js
+++ Penktadienio uzduotys/U8 -fibonnacio skaicius/script.js
@@ -2,17 +2,22 @@ window.onload=function(){
 
     let skaicius;
     let fibonacio;
-    skaicius=5;
+    skaicius=12;
+    skaicius1=78;
 
 
     fibonacio=fibonacioSkaicius(skaicius);
+    fibonacio1=fibonacioSkaicius(skaicius1);
 
     console.log(fibonacio);
+    console.log(fibonacio1);
 
-
+   
  
 
 }
 function fibonacioSkaicius(a){
-    return ( Math.pow( (1 + Math.sqrt(5))/2,a ))/Math.sqrt(5) ; 
+    return Math.round(( Math.pow( (1 + Math.sqrt(5))/2 ,a ))/Math.sqrt(5)) ; 
 }
+
+
